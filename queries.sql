bffe58715c8e37880919089835b63611cff3a40d
Zuwairanajma <juwairiyyasadiq@gmail.com>
2023-09-26 11:01:16 +0100

    Make required changes

--- queries.sql
+++ queries.sql
@@ -1,26 +1,21 @@
-/*Queries that provide answers to the questions from all projects.*/
-
-SELECT * from animals WHERE name LIKE '%mon';
-SELECT name from animals WHERE date_of_birth >= 'Dec 31 2016' AND date_of_birth <= 'Jan 01 2019';
-SELECT name from animals WHERE neutered = 'True' and escape_attempts < '3';
+SELECT * FROM animals WHERE name LIKE '%mon';
+SELECT name FROM animals WHERE date_of_birth >= 'Dec 31 2016' AND date_of_birth <= 'Jan 01 2019';
+SELECT name FROM animals WHERE neutered = 'True' and escape_attempts < '3';
 SELECT date_of_birth from animals WHERE name = 'Agumon' or name = 'Pikachu';
 SELECT name, escape_attempts from animals WHERE weight_kg > '10.5';
-SELECT * from animals WHERE neutered = 'True';
-SELECT * from animals WHERE name NOT IN ('Gabumon');
-SELECT * from animals WHERE weight_kg >= '10.4' and weight_kg <= '17.3';
+SELECT * FROM animals WHERE neutered = 'True';
+SELECT * FROM animals WHERE name NOT IN ('Gabumon');
+SELECT * FROM animals WHERE weight_kg >= '10.4' and weight_kg <= '17.3';
 
 -- Query and Update animals TABLE
 
 BEGIN TRANSACTION;
 
 UPDATE animals
-SET species = 'unspecified'
-WHERE species IS NULL;
-
+SET species = 'unspecified';
 SELECT * FROM animals;
-
 ROLLBACK;
-
+SELECT * FROM animals;
 BEGIN TRANSACTION;
 
 UPDATE animals
@@ -35,6 +30,7 @@ SELECT * FROM animals;
 
 COMMIT;
 
+SELECT * FROM animals;
 -- Begin Second Transaction
 
 BEGIN TRANSACTION;
@@ -75,11 +71,13 @@ WHERE escape_attempts = 0;
 SELECT AVG(weight_kg)
 FROM animals;
 
-SELECT neutered, COUNT(*) AS escape_count FROM animals
+SELECT neutered, MAX(escape_attempts) AS max_escape_count
+FROM animals
 WHERE escape_attempts > 0
 GROUP BY neutered
-ORDER BY escape_count DESC
+ORDER BY max_escape_count DESC
 LIMIT 1;
+
 
 SELECT species,
 MIN(weight_kg) AS min_weight,
